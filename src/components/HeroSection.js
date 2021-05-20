d294522a2d594f95708f93b27d5c0e7c66bfb6ff
Stackbit Code Editor <projects@stackbit.com>
2021-05-20 15:38:24 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,7 +14,9 @@ export default class HeroSection extends React.Component {
                 <h1 className="hero__title">{_.get(section, 'title', null)}</h1>
                 )}
                 {_.get(section, 'content', null) && (
-                
+                <div className="hero__body text-block">
+                  {markdownify(_.get(section, 'content', null))}
+                </div>
                 )}
                 {_.get(section, 'actions', null) && (
                 <div className="hero__actions button-group">

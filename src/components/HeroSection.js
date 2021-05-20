a01adb7b97282e2e3e58f2953b48732cf29d4aba
Stackbit Code Editor <projects@stackbit.com>
2021-05-20 15:36:50 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,9 +14,9 @@ export default class HeroSection extends React.Component {
                 <h1 className="hero__title">{_.get(section, 'title', null)}</h1>
                 )}
                 {_.get(section, 'content', null) && (
-                <div className="hero__body text-block">
-                  {markdownify(_.get(section, 'content', null))}
-                </div>
+                <iframe width="100%" height="400" src="https://time.graphics/ru/embed?v=1&id=508715" frameborder="0" allowfullscreen></iframe>
+                <div><a  style="font-size: 12px; text-decoration: none;" title="Milestone chart" href="https://time.graphics">Milestone chart</a></div>
+    
                 )}
                 {_.get(section, 'actions', null) && (
                 <div className="hero__actions button-group">

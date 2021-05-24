fd3dd85b98d4a97f9089afa0ad30bee651dbace3
Stackbit Code Editor <projects@stackbit.com>
2021-05-24 11:57:21 +0000

    init.js: changed

--- public/js/init.js
+++ public/js/init.js
@@ -51,10 +51,6 @@ window.removeMainNavigationHandlers = function() {
 	}
 };
 
-window.addEventListener('resize', function () {
-  if (document.querySelector('.js-nav-toggle').offsetParent === null) {
-    document.body.classList.remove('js-nav-open');
-  }
-}, true);
+
 
 };
